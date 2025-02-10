6c0f920a72cafcc41237c8470a10bd672fb20399
Clément Habinshuti <haby_habbes@live.com>
2025-02-10 20:33:54 +0300

    Build self-attention block

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Other resources:
 
 - [High-Resolution Image Synthesis with Latent Diffusion Models](https://arxiv.org/abs/1505.04597)
 - [U-Net: Convolutional Networks for Biomedical Image Segmentation](https://arxiv.org/abs/1505.04597)
+- [Transform from scratch](https://www.youtube.com/watch?v=bCz4OMemCcA)
 - [Deep Residual Learning for Image Recognition](https://arxiv.org/pdf/1512.03385)
 - [Generative Adversarial Nets](https://arxiv.org/pdf/1406.2661)
 - [An Introduction to Variational Autoencoders](https://arxiv.org/pdf/1906.02691)
--- sd/attention.py
+++ sd/attention.py
@@ -1,3 +1,60 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import math
 
-class SelfAttention:
-    pass
+# To learn more about attention: https://www.youtube.com/watch?v=bCz4OMemCcA&t=0s
+
+class SelfAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        # Each head will observe a subset of the input embedding
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x: torch.Tensor, causal_mask=False):
+        # x: (batch_size, seq_len, dim)
+        
+        input_shape = x.shape
+
+        batch_size, sequence_length, dim = input_shape
+
+        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
+
+        # (batch_size, seq_len, dim) -> (batch_size, seq_len, 3 * dim) -> 3 tensors of shape (batch_size, seq_len, dim)
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
+
+        # (batch_size, seq_len, dim) -> (batch_size, seq_len, n_heads, d_head) -> (batch_size, n_heads, seq_len, d_head)
+        q = q.view(interim_shape).transpose(1, 2)
+        k = k.view(interim_shape).transpose(1, 2)
+        v = v.view(interim_shape).transpose(1, 2)
+
+        # (batch_size, n_heads, seq_len, seq_len)
+        weight = q % k.transpose(-1, -2)
+
+        if causal_mask:
+            # Mask where the upper triangle (above the principal diagonal) is made up of 1
+            # Create a mask that prevents the model from looking at the future tokens
+            # Not sure if this is relevant for images since pixels are not ordered chronologically
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
+            weight.masked_fill(mask, -torch.inf)
+
+        weight /= math.sqrt(self.d_head)
+
+        weight = F.softmax(weight, dim=-1)
+
+        # (batch_size, n_heads, seq_len, seq_len) @ (batch_size, n_heads, seq_len, d_head) -> (batch_size, n_heads, seq_len, d_head)
+        output = weight @ v
+
+        # (batch_size, n_heads, seq_len, d_head) -> (batch_size, seq_len, n_heads, d_head)
+        output = output.transpose(1, 2)
+
+        output = output.reshape(input_shape)
+
+        output - self.out_proj(output)
+
+        # (batch_size, seq_len, dim)
+        return output
