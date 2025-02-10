12051bc13b4065bbb3de27412f428888b46bad92
Clément Habinshuti <haby_habbes@live.com>
2025-02-10 18:50:29 +0300

    Build Residual block

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Other resources:
 
 - [High-Resolution Image Synthesis with Latent Diffusion Models](https://arxiv.org/abs/1505.04597)
 - [U-Net: Convolutional Networks for Biomedical Image Segmentation](https://arxiv.org/abs/1505.04597)
+- [Deep Residual Learning for Image Recognition](https://arxiv.org/pdf/1512.03385)
 - [Generative Adversarial Nets](https://arxiv.org/pdf/1406.2661)
 - [An Introduction to Variational Autoencoders](https://arxiv.org/pdf/1906.02691)
 - [Diffusion from scratch](https://github.com/Animadversio/DiffusionFromScratch)
--- sd/decoder.py
+++ sd/decoder.py
@@ -1,7 +1,52 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention
 
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        # The residual block is made up of a convolutions and group normalizations
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        # The last layer will be a residual layer that adds the input to the output.
+        # But the in_channels might differ from out_channels. We create a helper
+        # residual layer to convert the input to the output shape if necessary.
+        if in_channels == out_channels:
+            # if the channels match, the residual layer won't perform any transformation
+            self.residual_layer = nn.Identity()
+        else:
+            # if the channels don't match, the residual layer will transform the input to match the output number of channels
+            # using a convolution
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (batch_size, in_channels, height, width)
+        
+        residue = x
+        
+        x = self.groupnorm_1(x)
+
+        x = F.silu(x)
+
+        x = self.conv_1(x)
+
+        x = self.groupnorm_2(x)
+
+        x = F.silu(x)
+
+        x = self.conv_2(x)
+
+        # In the residual layer we want to add the input to the output
+        # But since the input and output can have different shapes (difference in number of channels),
+        # we use the residual layer to transform the input to the output shape.
+        return x + self.residual_layer(residue)
+
+    
 
 class VAE_AttentionBlock:
-    pass
-
-class VAE_ResidualBlock:
     pass
